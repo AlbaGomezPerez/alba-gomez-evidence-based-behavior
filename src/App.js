a0259c22b1e68c29c1b6b0972e7d58535ed5965b
Alba Gómez Pérez <albagope@gmail.com>
2020-05-22 15:04:31 +0200

    Add className and remove unused code

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ class App extends React.Component {
             HoursStatistics: [],
             CitiesStatistics : []
         }
-
     }
 
     componentDidMount() {
@@ -44,12 +43,11 @@ class App extends React.Component {
 
 
     render () {
-        const {AllUsers} = this.state;
         return (
             <div className="container">
                 <nav>
-                    <div class="nav-wrapper">
-                        <a href="#" class="brand-logo">Mi app</a>
+                    <div className="nav-wrapper">
+                        <a href="#" className="brand-logo">Mi app</a>
                     </div>
                 </nav>
                     <main className="">
@@ -120,27 +118,27 @@ class App extends React.Component {
                             </table>
                     </main>
                     <footer className="page-footer">
-                                <div class="container">
-                                <div class="row">
-                                <div class="col l6 s12">
-                                <h5 class="white-text">Footer Content</h5>
-        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                                <div className="container">
+                                <div className="row">
+                                <div className="col l6 s12">
+                                <h5 className="white-text">Footer Content</h5>
+        <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
         </div>
-        <div class="col l4 offset-l2 s12">
-            <h5 class="white-text">Links</h5>
+        <div className="col l4 offset-l2 s12">
+            <h5 className="white-text">Links</h5>
             <ul>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+            <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+        <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+        <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+        <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
         </ul>
         </div>
         </div>
         </div>
-        <div class="footer-copyright">
-            <div class="container">
+        <div className="footer-copyright">
+            <div className="container">
             © 2014 Copyright Text
-        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+        <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
         </div>
         </div>
                     </footer>
